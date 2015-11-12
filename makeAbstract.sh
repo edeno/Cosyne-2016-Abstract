3b9cb61fd18b8badeeb3fb1ee9db7339c4088b8f
edeno <edeno@bu.edu>
2015-11-12 09:50:03 -0500

    Smaller margins

--- makeAbstract.sh
+++ makeAbstract.sh
@@ -2,7 +2,7 @@
 # Make cosyne abstract
 pandoc title-authors.md 300-Word-Summary.md additional-detail.md \
        -o public/edenovellis-cosyne-abstract-2016.pdf \
-       -V geometry:margin=1in \
+       -V geometry:margin=0.5in \
        -V fontsize=12pt \
        --filter pandoc-crossref \
        --smart
